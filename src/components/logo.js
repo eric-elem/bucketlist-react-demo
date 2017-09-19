39a07c9c0f9957c9e4cb226e83ed401e6e64b825
eric-elem <eric.elem@andela.com>
2017-09-19 14:56:41 +0300

    Add logo

---
+++ src/components/logo.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+    render () {
+        return (
+            <h1 className="logo text-center mt-5 mb-5">Bucketlist</h1>
+        );
+    }
+}
